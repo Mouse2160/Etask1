34d7f0832afe339407c75328a0498605d4d3e396
Kirill Romanchuk <kyrylo.romanchuk@gmail.com>
2019-02-07 23:35:45 +0200

    new changes

--- src/main/java/helloWorld/Controller.java
+++ src/main/java/helloWorld/Controller.java
@@ -1,28 +1,32 @@
 package helloWorld;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Controller {
     private Model model;
     private View view;
+    private List<String> wordsDictionary = new ArrayList<>();
 
     public Controller(Model model, View view) {
         this.model = model;
         this.view = view;
+        this.wordsDictionary.add("Hello");
+        this.wordsDictionary.add("World");
     }
 
     public void processUser() {
         Scanner text = new Scanner(System.in);
-        inputText(text, "Hello");
-        inputText(text, "World");
+        for (String checkWord : wordsDictionary) inputText(text, checkWord);
         view.viewMessageAndWords(View.CURRENT_TEXT, model.getWord());
     }
 
     private void inputText(Scanner in, String checkWord) {
         boolean continueProgram = true;
-        String currentWord;
 
         while (continueProgram) {
+            String currentWord;
             view.viewMessage(String.format(View.INPUT_WORD, checkWord));
             currentWord =  in.nextLine();
 
