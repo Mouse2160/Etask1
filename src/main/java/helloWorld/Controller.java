e31dc81680a5316c416af5cab4f29d5ad03bc8fc
Kirill Romanchuk <kyrylo.romanchuk@gmail.com>
2019-02-07 23:56:13 +0200

    add words dictionary, refactoring the code

--- src/main/java/helloWorld/Controller.java
+++ src/main/java/helloWorld/Controller.java
@@ -1,24 +1,22 @@
 package helloWorld;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
 public class Controller {
     private Model model;
     private View view;
-    private List<String> wordsDictionary = new ArrayList<>();
+    private List<String> wordsDictionary = Arrays.asList("Hello", "World");
 
     public Controller(Model model, View view) {
         this.model = model;
         this.view = view;
-        this.wordsDictionary.add("Hello");
-        this.wordsDictionary.add("World");
     }
 
     public void processUser() {
-        Scanner text = new Scanner(System.in);
-        for (String checkWord : wordsDictionary) inputText(text, checkWord);
+        Scanner inputText = new Scanner(System.in);
+        for (String checkWord : wordsDictionary) inputText(inputText, checkWord);
         view.viewMessageAndWords(View.CURRENT_TEXT, model.getWord());
     }
 
